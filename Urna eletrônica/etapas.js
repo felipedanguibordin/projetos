7ae18756653b78d0b05e5eff23ab965c4bfc54dd
felipedanguibordin <felipedanguibordin@gmail.com>
2022-10-20 17:19:07 -0300

    sdsd

---
+++ Urna eletrônica/etapas.js
@@ -0,0 +1,56 @@
+let etapas = [
+    {
+    titulo: 'PRESIDENTE',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: '22',
+            nome: 'JAIR MESSIAS BOLSONARO',
+            partido: 'PL',
+            foto:[
+                {url:'Bolsonaro.jpg', legenda: 'Presidente'}
+            ]
+        },
+        {
+            numero: '13',
+            nome: 'LUIZ INÁCIO LULA DA SILVA',
+            partido: 'PT',
+            foto:[
+                {url:'Lula.jpg', legenda: 'PRESOdente'}
+            ]
+        },
+        {
+            numero: '12',
+            nome: 'CIRO FERREIRA GOMES',
+            partido: 'PDT',
+            foto: [
+                {url: 'Ciro.jpg', legenda: 'Calvo'}
+            ]
+        },
+        {
+            numero: '14',
+            nome: 'PADRE KELMON',
+            partido: 'PTB',
+            foto: [
+                {url: 'Padre Kelmon.jpg', legenda: 'Padre✌🏻✌🏻'}
+            ]
+        },
+        {
+            numero: '99',
+            nome: 'DILMA VANA ROUSSEFF',
+            partido: 'PT',
+            foto: [
+                {url: 'Dilma.jpg', legenda: 'Mandioca'}
+            ]
+        },
+        {
+            numero: '10',
+            nome: 'NEYMAR JR',
+            Partido: 'BR',
+            foto: [
+                {url: 'Ney.jpg', legenda: 'Neymito'}
+            ]
+        }
+    ]
+    }
+]
