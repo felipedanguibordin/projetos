ee7577fdba6aa6b9b58f8881b45370e1d2577d3b
felipedanguibordin <felipedanguibordin@gmail.com>
2022-10-28 16:26:13 -0300

    f

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Pedro';
+
+  userData = {
+    email: 'pedrinhoreidelas777@gmail.com',
+    role: 'Adm',
+  }
+
   title = 'angular';
 }
