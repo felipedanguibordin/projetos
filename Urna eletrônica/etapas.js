ee2e6a228b8ddc8f5c56b8fca902c67f89ffcea3
felipedanguibordin <felipedanguibordin@gmail.com>
2022-12-15 15:43:07 -0300

    s

--- Urna eletrônica/etapas.js
+++ Urna eletrônica/etapas.js
@@ -1,56 +1,62 @@
 let etapas = [
-    {
-    titulo: 'PRESIDENTE',
+  {
+    titulo: "PRESIDENTE",
     numeros: 2,
     candidatos: [
-        {
-            numero: '22',
-            nome: 'JAIR MESSIAS BOLSONARO',
-            partido: 'PL',
-            foto:[
-                {url:'Bolsonaro.jpg', legenda: 'Presidente'}
-            ]
-        },
-        {
-            numero: '13',
-            nome: 'LUIZ INÁCIO LULA DA SILVA',
-            partido: 'PT',
-            foto:[
-                {url:'Lula.jpg', legenda: 'PRESOdente'}
-            ]
-        },
-        {
-            numero: '12',
-            nome: 'CIRO FERREIRA GOMES',
-            partido: 'PDT',
-            foto: [
-                {url:'Cirozada.jpg', legenda: 'Calvo'}
-            ]
-        },
-        {
-            numero: '14',
-            nome: 'PADRE KELMON',
-            partido: 'PTB',
-            foto: [
-                {url: 'Padre Kelmon.jpg', legenda: 'Padre✌🏻✌🏻'}
-            ]
-        },
-        {
-            numero: '99',
-            nome: 'DILMA VANA ROUSSEFF',
-            partido: 'PT',
-            foto: [
-                {url: 'Dilma.jpg', legenda: 'Mandioca'}
-            ]
-        },
-        {
-            numero: '10',
-            nome: 'NEYMAR JR',
-            partido: 'BR',
-            foto: [
-                {url: 'Ney.jpg', legenda: 'Neymito'}
-            ]
-        }
-    ]
-    }
-]
+      {
+        numero: "22",
+        nome: "JAIR MESSIAS BOLSONARO",
+        partido: "PL",
+        foto: [{ url: "Bolsonaro.jpg", legenda: "Presidente" }],
+      },
+      {
+        numero: "13",
+        nome: "LUIZ INÁCIO LULA DA SILVA",
+        partido: "PT",
+        foto: [{ url: "Lula.jpg", legenda: "PRESOdente" }],
+      },
+      {
+        numero: "12",
+        nome: "CIRO FERREIRA GOMES",
+        partido: "PDT",
+        foto: [{ url: "Cirozada.jpg", legenda: "Calvo" }],
+      },
+      {
+        numero: "14",
+        nome: "PADRE KELMON",
+        partido: "PTB",
+        foto: [{ url: "Padre Kelmon.jpg", legenda: "Padre✌🏻✌🏻" }],
+      },
+      {
+        numero: "99",
+        nome: "DILMA VANA ROUSSEFF",
+        partido: "PT",
+        foto: [{ url: "Dilma.jpg", legenda: "Mandioca" }],
+      },
+      {
+        numero: "10",
+        nome: "NEYMAR JR",
+        partido: "BR",
+        foto: [{ url: "Ney.jpg", legenda: "Neymito" }],
+      },
+      {
+        numero: "20",
+        nome: "DIOGO DEFANTE",
+        partido: "DD",
+        foto: [{ url: "defante.jpg", legenda: "simplismente diogo defante" }],
+      },
+      {
+        numero: "45",
+        nome: "MATHEUS",
+        partido: "KKK",
+        foto: [{ url: "", legenda: "Atheus da lava jato" }],
+      },
+      {
+        numero: "07",
+        nome: "Da Ronco",
+        partido: "BMB",
+        foto: [{ url: "daronco.jpg", legenda: "juiz armário" }],
+      },
+    ],
+  },
+];
