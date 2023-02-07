c5c4f520ed954d1e32d5a22c1a30604138a3f0ea
felipe.bordin <felipe.bordin@xpert.com.br>
2023-02-07 13:41:12 -0300

    io

--- projeto da senha/script.js
+++ projeto da senha/script.js
@@ -1,18 +1,17 @@
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
-let sizePasswor = document.querySelector("#valor");
-let passwor = document.querySelector("#password");
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
 
-let containerpassword = document.querySelector("#container-password");
+let containerPassword = document.querySelector("#container-password");
 
-let charset =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oniput = function () {
+slider.oninput = function () {
   sizePassword.innerHTML = this.value;
 };
 
@@ -22,8 +21,9 @@ function generatePassword() {
     pass += charset.charAt(Math.floor(Math.random() * n));
   }
 
+  console.log(pass);
   containerPassword.classList.remove("hide");
-  sizePassword.innerHTML = pass;
+  password.innerHTML = pass;
   novaSenha = pass;
 }
 
