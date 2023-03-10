006a0ff35b02a0efc9c10168f0121eaa46fc7f62
felipe.bordin <104600522+felipedanguibordin@users.noreply.github.com>
2023-03-10 17:29:29 -0300

    d

--- countdown/index.js
+++ countdown/index.js
@@ -1,31 +1,29 @@
-const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
-const minsEl = document.getElementById("mins");
-const secondsEl = document.getElementById("second");
+// Define a data e hora do ano novo
+var newYear = new Date("Oct 20, 2023 00:00:00").getTime();
 
-const newYears = "20 Oct 2023";
+// Atualiza a contagem regressiva a cada segundo
+var x = setInterval(function () {
+  // Obtém a data e hora atual
+  var now = new Date().getTime();
 
-function countdown() {
-  const newYearsDate = new Date(newYears);
-  const currentDate = new Date();
+  // Calcula a diferença entre a data e hora atual e o ano novo
+  var distance = newYear - now;
 
-  const totalSeconds = (newYearsDate - currentDate) / 1000;
+  // Calcula o tempo restante em dias, horas, minutos e segundos
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  const days = Math.floor(totalSeconds / 3600 / 24);
-  const hours = Math.floor((totalSeconds / 3600) % 24);
-  const mins = Math.floor((totalSeconds / 60) % 60);
-  const seconds = Math.floor(totalSeconds) % 60;
+  // Exibe a contagem regressiva no HTML
+  document.getElementById("days").innerHTML = days + " dias";
+  document.getElementById("hours").innerHTML = hours + " horas";
+  document.getElementById("minutes").innerHTML = minutes + " minutos";
+  document.getElementById("seconds").innerHTML = seconds + " segundos";
 
-  daysEl.innerHTML = days;
-  hoursEl.innerHTML = formatTime(hours);
-  minsEl.innerHTML = formatTime(mins);
-  secondsEl.innerHTML = formatTime(seconds);
-}
-
-function formatTime(time) {
-  return time < 10 ? `0${time}` : time;
-}
-
-countdown();
-
-setInterval(countdown, 1000);
+  // Quando a contagem regressiva chegar a zero, exibe uma mensagem
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Feliz Aniversário!";
+  }
+}, 1000);
